55c0d16c99a468eb9ddc15f661510499e57cb6d9
Reobotech Tecnologia <ti@reobotech.com>
2022-07-01 16:19:58 -0300

    First connection with firebase [Testing]

---
+++ backend/index.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+dbLink = "https://samubase-b839f-default-rtdb.firebaseio.com/"
+
+##POST
+
+    ##CRIAR NOVA ATIVIDADE
+data = {
+    "name": "Abertura de SITE",
+    "operator" : "Magno Rodolfo dos Santos Ribeiro",
+    "mobile": 88334422,
+    "date": "01/06/2022"
+}
+
+newActivityLink = f'{dbLink}/Atividades/.json'
+newActivityRequest= requests.post(newActivityLink, json.dump(data))
+    ##CRIAR NOVA OPERADORA
+
+
+##GET
+
+##PATCH
+
+##DELETE
+
+
+##TESTING API CONNECTION
+print(newActivityRequest)
+print(newActivityRequest.text)
+
