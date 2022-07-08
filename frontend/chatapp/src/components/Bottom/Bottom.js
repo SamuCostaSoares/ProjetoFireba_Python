1fd26601c1fb93b4e97c957a73976a0136d508b3
Reobotech Tecnologia <ti@reobotech.com>
2022-07-08 16:26:13 -0300

    Bottom Connection

---
+++ frontend/chatapp/src/components/Bottom/Bottom.js
@@ -0,0 +1,22 @@
+import React, { Bottom } from "@mui/material";
+
+export default function BottomNavegation (){
+    const 
+        [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
