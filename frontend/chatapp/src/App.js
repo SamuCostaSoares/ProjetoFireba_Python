68cac661152bad7264333c68be26fcb043c93793
Reobotech Tecnologia <ti@reobotech.com>
2022-07-08 12:06:37 -0300

    Login rules implementation

--- frontend/chatapp/src/App.js
+++ frontend/chatapp/src/App.js
@@ -6,27 +6,42 @@ import './App.css';
 import CircularIndeterminate from './components/Loading/loading';
 import Login from './components/Login/login';
 import { Container, Typography, Box } from "@mui/material";
+import checkValidUser from "./services/checkValidUser";
 
 function App() {
-  const [user, loading ] = useAuthState(auth);
+  const 
+    [user, loading ] = useAuthState(auth),
+    [userAuthorized, setUserAuthorized] = useState(false);
   useEffect(() => {
-    console.log("USER:",user);
-    if(user){
-      db.collection("users").doc(user.uid).set({
-        email: user.email,
-        photoURL: user.photoURL
-      })
+    let p1 = new Promise((resolve, reject) => {
+      if(user){
+        db.collection("users").doc(user.uid).set({
+          email: user.email,
+          photoURL: user.photoURL
+        });
+        resolve(user.email)
+      }else{
+        reject("Failed")
+      }
     }
+    );
+    p1.then((val) => {
+      checkValidUser(db, val).then(ret => {
+        var userPermissionCheckResult = ret;
+        setUserAuthorized(userPermissionCheckResult);
+      });
+    })
   }, [user]);
 
-  if(!user) return <Login />
+  console.log("userAuthorized state",userAuthorized);
+  if(!user || !userAuthorized) return <Login />
 
   if(loading) return <CircularIndeterminate />
 
   return (
     <div className="App">
-      <AppHeader></AppHeader>
-      <AppSidebar></AppSidebar>
+      {/* <AppHeader></AppHeader> */}
+      {/* <AppSidebar></AppSidebar> */}
       <Container maxWidth="md">
         <Box sx={{ bgcolor: '#BEDDEF', height: '100vh' }} >
           <Typography variant="h1" component="div">Tela PRINCIPAL DO APP</Typography>
---
+++ frontend/chatapp/src/services/checkValidUser.js
@@ -0,0 +1,16 @@
+import  { collection, query, where, getDocs } from "firebase/firestore";
+
+export default async function checkValidUser (db, userEmail) {
+    let userAuthorized = false;
+    const permissionCollectionRef = collection(db, "Permissions"); //collection reference
+    const q = query(permissionCollectionRef)
+    // console.log("Permissions from Firestore", permissionCollection);
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        if(userEmail === doc.id){
+            alert(`O usuário tem permissão! ${userEmail}`);
+            userAuthorized = true
+        }
+    })
+    return userAuthorized;
+}
